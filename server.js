c4b3bc6a91537d2c499a206a67c57a54454eeecc
caiohenriquesa1@gmail.com <caiohenriquesa1@gmail.com>
2024-12-06 23:14:37 -0300

    start basic nft minting app based off cse340 template

--- server.js
+++ server.js
@@ -7,8 +7,10 @@
  *************************/
 const express = require("express")
 const env = require("dotenv").config()
+const path = require("path");
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
 
 /* ***********************
  * Routes
@@ -21,6 +23,17 @@ app.use(static)
  *************************/
 const port = process.env.PORT
 const host = process.env.HOST
+
+// Set the views directory and template engine
+app.set("views", path.join(__dirname, "views")); // Specify the "views" folder
+app.set("view engine", "ejs"); // Set EJS as the default template engine
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+// Route for the home page
+app.get("/", (req, res) => {
+  res.render("index"); // This renders the "views/index.ejs" file
+});
 
 /* ***********************
  * Log statement to confirm server operation
